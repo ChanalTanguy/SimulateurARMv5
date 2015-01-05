c7c38896398d996206e6fa551813a256eebb8c51
chanalt <chanalt@F202-10.e-im2ag.ujf-grenoble.fr>
2015-01-05 17:05:55 +0100

    memory_read_byte

--- Simulateur/memory.c
+++ Simulateur/memory.c
@@ -56,7 +56,11 @@ void memory_destroy(memory mem) {
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
-    return -1;
+    if (address > (mem->size)-1){
+		return -1;
+	}
+	*value = *(mem->address + address);
+	return 0;
 }
 
 int memory_read_half(memory mem, int be, uint32_t address, uint16_t *value) {
