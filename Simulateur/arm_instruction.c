28632206c308e8f93dfe82f0d5c4a31d51dd8b97
ChanalTanguy <tanguy.chanal@iut-valence.fr>
2015-01-07 13:42:26 +0100

    Update arm_instruction.c

--- Simulateur/arm_instruction.c
+++ Simulateur/arm_instruction.c
@@ -1,19 +1,19 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T Ã  but pÃ©dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique GÃ©nÃ©rale GNU publiÃ©e par la Free Software
+Foundation (version 2 ou bien toute autre version ultÃ©rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuÃ© car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spÃ©cifique. Reportez-vous Ã  la
+Licence Publique GÃ©nÃ©rale GNU pour plus de dÃ©tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reÃ§u une copie de la Licence Publique GÃ©nÃ©rale GNU en mÃªme
+temps que ce programme ; si ce n'est pas le cas, Ã©crivez Ã  la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+Ã‰tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
          ENSIMAG - Laboratoire LIG
@@ -31,15 +31,15 @@ Contact: Guillaume.Huard@imag.fr
 static int arm_execute_instruction(arm_core p) {
 	uint32_t instr;
 	uint32_t adr_pc;
-	adr_pc = arm_read_register(p, 15); // On récupère l'adresse contenue dans le PC (15 = Registre correspondant au PC)
-	if (arm_read_word(p, adr_pc, &instr) != 0){ // On lit l'instruction contenue à l'adresse récupérée
+	adr_pc = arm_read_register(p, 15); // On rÃ©cupÃ¨re l'adresse contenue dans le PC (15 = Registre correspondant au PC)
+	if (arm_read_word(p, adr_pc, &instr) != 0){ // On lit l'instruction contenue Ã  l'adresse rÃ©cupÃ©rÃ©e
 		return -1;
 	}
-	arm_write_register(p, 15, adr_pc+4); // On incrémente l'adresse contenue dans le PC de 4
+	arm_write_register(p, 15, adr_pc+2); // On incrÃ©mente l'adresse contenue dans le PC de 4
 
 	uint8_t cond;
 
-	uint32_t flags = arm_read_cpsr(p); // Récupération du CPSR 
+	uint32_t flags = arm_read_cpsr(p); // RÃ©cupÃ©ration du CPSR 
 	
 	// Le CPSR contient les flags ZNCV : http://www.heyrick.co.uk/armwiki/The_Status_register
 	
@@ -48,7 +48,7 @@ static int arm_execute_instruction(arm_core p) {
 	int c = get_bit(flags, 29);
 	int v = get_bit(flags, 28);
 
-	cond = get_bits(instr, 31, 28); // Récupération du code condition de l'instruction
+	cond = get_bits(instr, 31, 28); // RÃ©cupÃ©ration du code condition de l'instruction
 	int res;
 	
 	switch (cond) {
@@ -72,7 +72,7 @@ static int arm_execute_instruction(arm_core p) {
 	}
 
 	if (res) {
-		// Exécuter l'instruction
+		// ExÃ©cuter l'instruction
 	}
 	
 	return 0;
