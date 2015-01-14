495c109998e39ad30a6594aba300e289138259cf
ginetm <ginetm@F201-07.e-im2ag.ujf-grenoble.fr>
2015-01-14 11:25:57 +0100

    new load store

--- Simulateur/arm_load_store.c
+++ Simulateur/arm_load_store.c
@@ -27,217 +27,147 @@ Contact: Guillaume.Huard@imag.fr
 #include "debug.h"
 
 int arm_load_store(arm_core p, uint32_t ins) {
-  uint8_t masque = 0;
-  uint8_t rn;
-  uint8_t rd;
-  uint8_t rm;
-  uint32_t addresse;
-  uint32_t value;
-  uint32_t offset=0;
-
+	uint8_t masque = 0;
+	uint8_t rn;
+	uint8_t rd;
+	uint8_t rm;
+	uint8_t shit;
+	uint32_t addresse;
+	uint32_t value;
+	uint32_t offset=0;
+	
+	
 	rn = get_bits(ins,19,16);
 	rd = get_bits(ins,15,12);
 	adresse = arm_read_register( p,rn);
 	value = arm_read_register(p,rd);
-	masque = get_bits(ins,27,26);
-  
-if (get_bits(ins,27,26) == 0)
-    { //ldrH et strH
-		if(get_bit(ins,22)==0)
-		{}
-		else
-		{}
-     
-    }
-  else{
-  	 // ldr, ldrb, str, strb
-		if (get_bit(ins,20) == 0)//L
-		{ //str et strb 
-			if (get_bit(ins,22) == 0) //B
-			{//str	
-				if (get_bit(ins,25)==0)// I
-					{ 
-					offset = gets_bits(ins,11,0);
-					offset_op(ins,adresse);
-					arm_write_word(p,rn,adresse);
-					}
-				else 
-					{
-					if (get_bit(ins,24)==0)// P
-						{//str post indexed adressing
-						if (get_bit(ins,21)==0)//W
-							{// str normal memory acces
-							offset = gets_bits(ins,11,0);
-							arm_write_word( p,address,value);
-							offset_op(ins,adresse);
-							arm_write_register(p,rn,adresse);
-							}
-						else 
-						{return UNDEFINED_INSTRUCTION; }
-						}
-					else//p==1
-						{
-						if ( get_bit(ins,21)==0)//W
-							{//offset adressing
-							rm = gets_bits(ins,3,0);
-							offset = arm_read_register( p,rm);
-							offset_op(ins,adresse);
-							arm_write_word( p,address,value);
-							}
-						else
-							{// W= 1 et P=1 pre index addressing
-							rm = gets_bits(ins,3,0);
-							offset = arm_read_register( p,rm);
-							offset_op(ins,adresse);
-							rn = arm_write_register(p,adresse);
-							arm_write_word( p,address,value);
-							}
-						}
-					}
-	      else
-			{//strb
-				if (get_bit(ins,25)==0)// I
-					{ 
-					offset = gets_bits(ins,11,0);
-					offset_op(ins,adresse);
-					arm_write_byte(p,rn,adresse);
-					}
-				else
-					{
-					if  (get_bit(ins,24)==0)// P
-						{//str post indexed adressing
-						if (get_bit(ins,21)==0)//W
-							{// str normal memory acces(
-							offset = gets_bits(ins,11,0);
-							arm_write_bytes( p,address,value);
-							offset_op(ins,adresse);
-							arm_write_register(p,rn,adresse);
-							}
-						else 
-							{return UNDEFINED_INSTRUCTION; }
-						}
-					else//p==1
-						{
-						if ( get_bit(ins,21)==0)//W
-							{//offset adressing
-							rm = gets_bits(ins,3,0);
-							offset = arm_read_register( p,rm);
-							offset_op(ins,adresse);
-							arm_write_byte( p,address,value);
-							}
-						else
-							{// W= 1 et P=1 pre index addressing
-							rm = gets_bits(ins,3,0);
-							offset = arm_read_register( p,rm);
-							offset_op(ins,adresse);
-							rn = arm_write_register(p,adresse);
-							arm_write_byte( p,address,value);
-							}
-						}
-					}
-			}
-			}
-		else
-			{ //ldr et ldrb 
-				if (get_bit(ins,22) == 0) //B
-					{//ldr	
-					if (get_bit(ins,25)==0)// I
-						{ 
-						offset = gets_bits(ins,11,0);
-						offset_op(ins,adresse);
-						arm_write_word(p,rn,adresse);
-						}
-					else 
-						{
-						if (get_bit(ins,24)==0)// P
-							{//ldr post indexed adressing
-							if (get_bit(ins,21)==0)//W
-								{// ldr normal memory acces
-								offset = gets_bits(ins,11,0);
-								arm_write_word( p,address,value);
-								offset_op(ins,adresse);
-								arm_write_register(p,rn,adresse);
-								}
-							else 
-								{return UNDEFINED_INSTRUCTION; }
-							}
-						else//p==1
-							{
-								if ( get_bit(ins,21)==0)//W
-									{//offset adressing
-									rm = gets_bits(ins,3,0);
-									offset = arm_read_register( p,rm);
-									offset_op(ins,adresse);
-									arm_write_word( p,address,value);
-									}
-								else
-									{// W= 1 et P=1 pre index addressing
-									rm = gets_bits(ins,3,0);
-									offset = arm_read_register( p,rm);
-									offset_op(ins,adresse);
-									rn = arm_write_register(p,adresse);
-									arm_write_word( p,address,value);
-									}
-							}
-						}
-					}
-				else
-					{//ldrb
-					if (get_bit(ins,25)==0)// I
-						{ 
-						offset = gets_bits(ins,11,0);
-						offset_op(ins,adresse);
-						arm_write_byte(p,rn,adresse);
-						}
-					else
-						{
-						if  (get_bit(ins,24)==0)// P
-							{//ldrb post indexed adressing
-							if (get_bit(ins,21)==0)//W
-								{// ldrb normal memory acces(
-								offset = gets_bits(ins,11,0);
-								arm_write_bytes( p,address,value);
-								offset_op(ins,adresse);
-								arm_write_register(p,rn,adresse);
-								}
-							else 
-								{return UNDEFINED_INSTRUCTION; }
-							}
-						else//p==1
-							{
-								if ( get_bit(ins,21)==0)//W
-									{//offset adressing
-									rm = gets_bits(ins,3,0);
-									offset = arm_read_register( p,rm);
-									offset_op(ins,adresse);
-									arm_write_byte( p,address,value);
-									}
-								else
-									{// W= 1 et P=1 pre index addressing
-									rm = gets_bits(ins,3,0);
-									offset = arm_read_register( p,rm);
-									offset_op(ins,adresse);
-									rn = arm_write_register(p,adresse);
-									arm_write_byte( p,address,value);
-									}
-							}
-						}
-					}
-			}
-}
 
-uint32_t offset_op(uint32_t ins,uint32_t adresse){
-				if ( get_bit(ins,23)==0)//U
-				{  return adresse-offset;}
-			else 
-				{return adresse+offset;}
+if (get_bits(ins,27,26) == 0)
+	{ //ldrH et strH
+	}
+else
+	{ // ldr, ldrb, str, strb
+	if (get_bit(ins,25)==0)// I
+		{offset = get_bits(ins,11,0);}
+	else 
+		{
+		rm = gets_bits(ins,3,0);
+		offset = arm_read_register( p,rm);
+			if ( get_bit(ins,11,4)!=0 )
+			{ shit =get_bit(ins,6,5);
 			}
+			
+		}
+	if (get_bit(ins,24)==0)// P
+		{//P=0 post indexed adressing
+			if (get_bit(ins,21)==0)//W
+				{//W=0 normal memory acces
+					if (get_bit(ins,20) == 0)//L
+						{// L=0 str et strb
+						if (get_bit(ins,22) == 0) //B	
+							{//B=0 str
+							arm_write_word( p,address,value);
+							}
+							else
+							{// B=1 strb
+							arm_write_byte( p,address,value);
+							}
+					else 
+					{ // L=1 ldr et ldrb
+						if (get_bit(ins,22) == 0) //B	
+							{//B=0 ldr
+							arm_read_word( p,address,value);
+							}
+							else
+							{// B=1 ldrb
+							arm_read_byte( p,address,value);
+							}
+					}
+				}
+			else
+			{// W=1 
+				return UNDEFINED_INSTRUCTION; 
+			}
+		
+		adresse=offset_op(ins,adresse,offset);
+		arm_write_register(p,rn,adresse);		
+		}
+	else 
+		{//P=1 offset adressing ou Pre-adrressing
+		if (get_bit(ins,21)==0)//W
+			{//W=0 offset adressing
+			adresse=offset_op(ins,adresse,offset);
+			if (get_bit(ins,20) == 0)//L
+				{// L=0 str et strb
+				if (get_bit(ins,22) == 0) //B	
+					{//B=0 str
+					arm_write_word( p,address,value);
+					}
+				else
+					{// B=1 strb
+						arm_write_byte( p,address,value);
+						}
+				}
+			else 
+				{ // L=1 ldr et ldrb
+				if (get_bit(ins,22) == 0) //B	
+					{//B=0 ldr
+					arm_read_word( p,address,value);
+					}
+				else
+					{// B=1 ldrb
+					arm_read_byte( p,address,value);
+					}
+				}
+			}
+		else
+			{// W=1 pre-addressing
+			adresse=offset_op(ins,adresse,offset);
+			arm_write_register(p,rn,adresse);
+			if (get_bit(ins,20) == 0)//L
+				{// L=0 str et strb
+				if (get_bit(ins,22) == 0) //B	
+					{//B=0 str
+					arm_write_word( p,address,value);
+					}
+				else
+					{// B=1 strb
+					arm_write_byte( p,address,value);
+					}
+				}
+			else 
+				{ // L=1 ldr et ldrb
+				if (get_bit(ins,22) == 0) //B	
+					{//B=0 ldr
+					arm_read_word( p,address,value);
+					}
+				else
+					{// B=1 ldrb
+					arm_read_byte( p,address,value);
+					}
+				}
+			} 
+		}		
+	}
+//fin ldr,ldrb, str, strb	
+}
+    return 1;
+}
 
 int arm_load_store_multiple(arm_core p, uint32_t ins) {
-  return UNDEFINED_INSTRUCTION;
+    return UNDEFINED_INSTRUCTION;
 }
-  
+
 int arm_coprocessor_load_store(arm_core p, uint32_t ins) {
-  /* Not implemented */
-  return UNDEFINED_INSTRUCTION;
+    /* Not implemented */
+    return UNDEFINED_INSTRUCTION;
 }
+
+uint32_t offset_op(uint32_t ins,uint32_t adresse,uint32_t offset){
+	if ( get_bit(ins,23)==0)//U
+		{  return adresse-offset;}
+	else 
+		{return adresse+offset;}
+}
+
+
